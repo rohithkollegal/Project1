174cd7c110ff81f7cacd275a1b3578d33dadcda9
04877D744 <04877D744@RNAGASUN>
2021-08-19 16:28:06 +0530

    SKUID A calculation

--- MaerskAssignment01/src/com/rohith/engine/PromotionEngine.java
+++ MaerskAssignment01/src/com/rohith/engine/PromotionEngine.java
@@ -1,12 +1,29 @@
 package com.rohith.engine;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class PromotionEngine {
 	
 	 public double promotionCalculation(HashMap<String,Integer> cart ) {
 		 
-		 return 0.0;
+		 double totalamount = 0; 
+		 Iterator<Entry<String, Integer>> itr = cart.entrySet().iterator();
+		 
+		 while(itr.hasNext()) {
+			 Map.Entry<String, Integer> entry = itr.next();
+			 String skuid = entry.getKey();
+			 int quantity = entry.getValue();
+			 
+			 if(skuid.equals("A")) {				
+				 totalamount = (quantity/3) * 130 + (quantity%3) * 50;		 		 
+			 }
+			 
+		 }
+		 
+		 return totalamount;
 	 }
 
 }
--- MaerskAssignment01/src/com/rohith/testcase/TestPromotionEngine.java
+++ MaerskAssignment01/src/com/rohith/testcase/TestPromotionEngine.java
@@ -1,9 +1,21 @@
 package com.rohith.testcase;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.rohith.engine.PromotionEngine;
+
 public class TestPromotionEngine {
+	
+	PromotionEngine promotionEngine = new PromotionEngine();
+	
   @Test
-  public void f() {
+  public void TestSkuidA() {
+	  HashMap<String,Integer> cart = new HashMap<String,Integer>();
+	  cart.put("A",5);
+	  Assert.assertEquals(230.00,promotionEngine.promotionCalculation(cart)); 	  
   }
 }

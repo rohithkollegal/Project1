65ab66c484564457390c8574d4abe8753be70a7e
04877D744 <04877D744@RNAGASUN>
2021-08-19 16:58:49 +0530

    updated

--- MaerskAssignment01/src/com/rohith/engine/PromotionEngine.java
+++ MaerskAssignment01/src/com/rohith/engine/PromotionEngine.java
@@ -11,6 +11,8 @@ public class PromotionEngine {
 		 
 		 double totalamount = 0; 
 		 Iterator<Entry<String, Integer>> itr = cart.entrySet().iterator();
+		 long skuidCcount = 0;
+		 long skuidDcount = 0;
 		 
 		 while(itr.hasNext()) {
 			 Map.Entry<String, Integer> entry = itr.next();
@@ -21,8 +23,31 @@ public class PromotionEngine {
 				 totalamount = (quantity/3) * 130 + (quantity%3) * 50;		 		 
 			 }
 			 
+			 if(skuid.equals("B")) {				
+				 totalamount = (quantity/2) * 45 + (quantity%2) * 30;
+			 }
+			 
+			 //since skuid C and D has to be calculated together, just collect the quantity of these two SKUIDs
+			 if(skuid.equals("C")) {				
+				 skuidCcount =quantity;
+			 }
+			 
+			 if(skuid.equals("D")) {				
+				 skuidDcount =quantity;
+			 }
 		 }
 		 
+		 if(skuidCcount == skuidDcount ) {
+			 totalamount += skuidCcount * 30 ;
+		 }
+		 if(skuidCcount>skuidDcount) {
+			 totalamount += skuidDcount* 30 + ((skuidCcount-skuidDcount) * 20);
+		 }
+		 if(skuidCcount<skuidDcount) {
+			 totalamount += skuidCcount* 30 + ((skuidDcount-skuidCcount) * 15);
+		 }
+		 
+		 System.out.println("C count :"+skuidCcount + " D count :"+skuidDcount);
 		 return totalamount;
 	 }
 
--- MaerskAssignment01/src/com/rohith/testcase/TestPromotionEngine.java
+++ MaerskAssignment01/src/com/rohith/testcase/TestPromotionEngine.java
@@ -16,6 +16,44 @@ public class TestPromotionEngine {
   public void TestSkuidA() {
 	  HashMap<String,Integer> cart = new HashMap<String,Integer>();
 	  cart.put("A",5);
-	  Assert.assertEquals(230.00,promotionEngine.promotionCalculation(cart)); 	  
+	  Assert.assertEquals(230.00,promotionEngine.promotionCalculation(cart)); 
+  }
+  
+  @Test
+  public void TestSkuidB() {
+	  HashMap<String,Integer> cart = new HashMap<String,Integer>();
+	  cart.put("B",5);
+	  Assert.assertEquals(120.00,promotionEngine.promotionCalculation(cart)); 	 
+	  cart.clear();
+	  cart.put("B", 0);
+	  Assert.assertEquals(0,promotionEngine.promotionCalculation(cart)); 
+	  
+	  //negatvie test case
+	  cart.clear();
+	  cart.put("B", 2);
+	  Assert.assertNotEquals(60.00, cart);
+  }
+  
+  @Test
+  public void TestSkuidC() {
+	  HashMap<String,Integer> cart = new HashMap<String,Integer>();
+	 
+	  cart.put("C",5);	  
+	  Assert.assertEquals(100.00,promotionEngine.promotionCalculation(cart)); 
+	  cart.clear();
+	  cart.put("C", 0);
+	  Assert.assertEquals(0,promotionEngine.promotionCalculation(cart)); 
+  }
+  
+  @Test
+  public void TestSkuidD() {
+	  HashMap<String,Integer> cart = new HashMap<String,Integer>();
+	 
+	  cart.put("D",5);	  
+	  System.err.println(promotionEngine.promotionCalculation(cart));
+	  Assert.assertEquals(75.00,promotionEngine.promotionCalculation(cart)); 
+	  cart.clear();
+	  cart.put("D", 0);
+	  Assert.assertEquals(0,promotionEngine.promotionCalculation(cart)); 
   }
 }

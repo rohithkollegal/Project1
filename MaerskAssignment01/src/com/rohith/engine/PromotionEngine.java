2c6653a01bcc28778d44a50991814e7191706e26
04877D744 <04877D744@RNAGASUN>
2021-08-19 15:44:28 +0530

    First Commit

---
+++ MaerskAssignment01/src/com/rohith/engine/PromotionEngine.java
@@ -0,0 +1,12 @@
+package com.rohith.engine;
+
+import java.util.HashMap;
+
+public class PromotionEngine {
+	
+	 public double promotionCalculation(HashMap<String,Integer> cart ) {
+		 
+		 return 0.0;
+	 }
+
+}
---
+++ MaerskAssignment01/src/com/rohith/testcase/TestPromotionEngine.java
@@ -0,0 +1,9 @@
+package com.rohith.testcase;
+
+import org.testng.annotations.Test;
+
+public class TestPromotionEngine {
+  @Test
+  public void f() {
+  }
+}
